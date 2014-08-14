27f874c820a55ec23df20ca16e2bcc26addcc929
julietzhu <724434852@qq.com>
2014-08-14 17:54:36 +0800

    当input和answer中数字全部相等，位置全不同返回0A4B

--- spec/NumberGameSpec.js
+++ spec/NumberGameSpec.js
@@ -2,13 +2,17 @@
  * Created by julia on 14-8-14.
  */
 describe("NumberGameSpec",function(){
-    var compare = new CompareNumber();
+    var compare ;
 
-//    beforeEach(function(){
-//        compare = new CompareNumber();
-//    });
+    beforeEach(function(){
+        compare = new CompareNumber();
+    });
 
-    it("should be printed 4A0B if input = answer",function(){
+    it("should print 4A0B if input = answer",function(){
         expect(compare.compareNumber("1234","1234")).toBe("4A0B");
     });
+
+    it("should print 0A4B if number equal but position is different",function(){
+        expect(compare.compareNumber("1234","4321")).toBe("0A4B");
+    })
 })
--- src/NumberGame.js
+++ src/NumberGame.js
@@ -5,7 +5,33 @@ function CompareNumber(){
 }
 
 CompareNumber.prototype.compareNumber = function(input,answer){
-    if(input==answer){
-        return "4A0B";
+    var countA = 0;
+    var countB = 0;
+
+    var positionA ;
+    var positionB ;
+
+    if(input===answer){
+        countA = 4;
+        countB = 0;
     }
+    else{
+        for(positionA=0;positionA<4;positionA++){
+            var numberInput = input[positionA];
+            console.log(positionA+"input"+numberInput);
+            for(positionB=0;positionB<4;positionB++){
+                var numberAnswer = answer[positionB];
+                console.log(positionB+"answer"+numberAnswer);
+                if(numberAnswer==numberInput){
+                    countB++;
+                    console.log("a"+countA);
+                    if(positionA==positionB){
+                        countA++;
+                    }
+                }
+            }
+        }
+    }
+
+    return countA+"A"+countB+"B";
 }
