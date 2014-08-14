e3b9028b9be795ee58c4880272fd175c05cf497e
julietzhu <724434852@qq.com>
2014-08-14 17:24:41 +0800

    当input=answer时返回4A0B

--- spec/NumberGameSpec.js
+++ spec/NumberGameSpec.js
@@ -1,3 +1,14 @@
 /**
  * Created by julia on 14-8-14.
  */
+describe("NumberGameSpec",function(){
+    var compare = new CompareNumber();
+
+//    beforeEach(function(){
+//        compare = new CompareNumber();
+//    });
+
+    it("should be printed 4A0B if input = answer",function(){
+        expect(compare.compareNumber("1234","1234")).toBe("4A0B");
+    });
+})
--- src/NumberGame.js
+++ src/NumberGame.js
@@ -1,3 +1,11 @@
 /**
  * Created by julia on 14-8-14.
  */
+function CompareNumber(){
+}
+
+CompareNumber.prototype.compareNumber = function(input,answer){
+    if(input==answer){
+        return "4A0B";
+    }
+}
