719291c687926d92d6fc12dd3ca56330afcf2eb3
julietzhu <724434852@qq.com>
2014-08-15 10:07:57 +0800

    完成当数字全部相等但是位置全不等输出044B测试

--- src/NumberGame.js
+++ src/NumberGame.js
@@ -8,30 +8,17 @@ CompareNumber.prototype.compareNumber = function(input,answer){
     var countA = 0;
     var countB = 0;
 
-    var positionA ;
-    var positionB ;
-
-    if(input===answer){
-        countA = 4;
-        countB = 0;
-    }
-    else{
-        for(positionA=0;positionA<4;positionA++){
-            var numberInput = input[positionA];
-            console.log(positionA+"input"+numberInput);
-            for(positionB=0;positionB<4;positionB++){
-                var numberAnswer = answer[positionB];
-                console.log(positionB+"answer"+numberAnswer);
-                if(numberAnswer==numberInput){
-                    countB++;
-                    console.log("a"+countA);
-                    if(positionA==positionB){
-                        countA++;
-                    }
-                }
+    for( var positionA=0;positionA<4;positionA++){
+        var numberInput = input[positionA];
+        for(var positionB=0;positionB<4;positionB++){
+            var numberAnswer = answer[positionB];
+            if(numberAnswer==numberInput&&positionA==positionB){
+                countA++;
+            }
+            if(numberAnswer==numberInput&&positionA!=positionB){
+                countB++;
             }
         }
     }
-
     return countA+"A"+countB+"B";
 }
