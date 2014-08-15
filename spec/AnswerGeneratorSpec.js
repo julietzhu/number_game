0bd3c73e930d9eaf927c725557392ecfdcc5497d
julietzhu <724434852@qq.com>
2014-08-15 15:31:36 +0800

    第二问完成判断重复测试

--- spec/AnswerGeneratorSpec.js
+++ spec/AnswerGeneratorSpec.js
@@ -3,15 +3,28 @@
  */
 describe("AnswerGeneratorSpec", function(){
     var answerGenerator;
+    var number;
 
     beforeEach(function(){
         answerGenerator = new AnswerGenerator();
-        var number = answerGenerator.setNumber();
+        number = answerGenerator.setNumber();
     });
 
     it("should print 4-digits", function(){
-
         expect(number.length).toBe(4);
     });
-    
+
+    it("the digits of answer are not repeat",function(){
+        var result = function(){
+            for(var i=0;i<4;i++){
+                for(var n=(i+1);n<4;n++){
+                    if(number[i]==number[n]){
+                        return "the digits exist repeat";
+                    }
+                }
+            }
+            return "the digits don't exist repeat"
+        }
+       expect(result()).toBe("the digits don't exist repeat");
+    });
 })
